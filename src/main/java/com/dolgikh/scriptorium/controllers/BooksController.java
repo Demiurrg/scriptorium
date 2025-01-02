8d5a0f74006aa3e45309ab1393c32809fa1dcac2
Demiurg <forge0047@gmail.com>
2025-01-02 17:10:25 +0300

    Create books controller with CRUD operations

---
+++ src/main/java/com/dolgikh/scriptorium/controllers/BooksController.java
@@ -0,0 +1,60 @@
+package com.dolgikh.scriptorium.controllers;
+
+import com.dolgikh.scriptorium.models.Book;
+import com.dolgikh.scriptorium.services.BooksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/books")
+public class BooksController {
+    private final BooksService booksService;
+
+    @Autowired
+    public BooksController(BooksService booksService) {
+        this.booksService = booksService;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("books", booksService.findAll());
+        return "books/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("book", booksService.findOne(id));
+        return "books/show";
+    }
+
+    @GetMapping("/new")
+    public String newBook(@ModelAttribute("book") Book book) {
+        return "books/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("book") Book book) {
+        booksService.save(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model) {
+        model.addAttribute("book", booksService.findOne(id));
+        return "books/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("book") Book book) {
+        booksService.save(book);
+        return "redirect:/books";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        booksService.delete(id);
+        return "redirect:/books";
+    }
+}
