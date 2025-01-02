dbe192044b46ece35641b525ff5720e55a9ae841
Demiurg <forge0047@gmail.com>
2025-01-02 16:54:17 +0300

    Create books repository and service with CRUD operations

---
+++ src/main/java/com/dolgikh/scriptorium/repositories/BooksRepository.java
@@ -0,0 +1,9 @@
+package com.dolgikh.scriptorium.repositories;
+
+import com.dolgikh.scriptorium.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BooksRepository extends JpaRepository<Book, Integer> {
+}
---
+++ src/main/java/com/dolgikh/scriptorium/services/BooksService.java
@@ -0,0 +1,38 @@
+package com.dolgikh.scriptorium.services;
+
+import com.dolgikh.scriptorium.models.Book;
+import com.dolgikh.scriptorium.repositories.BooksRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class BooksService {
+    private final BooksRepository booksRepository;
+
+    @Autowired
+    public BooksService(BooksRepository booksRepository) {
+        this.booksRepository = booksRepository;
+    }
+
+    public List<Book> findAll() {
+        return booksRepository.findAll();
+    }
+
+    public Book findOne(int id) {
+        return booksRepository.findById(id).orElse(null);
+    }
+
+    @Transactional
+    public void save(Book book) {
+        booksRepository.save(book);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        booksRepository.deleteById(id);
+    }
+}
