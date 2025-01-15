382fafe64582632096aec021f3eeab223022f6c4
Demiurrg <forge0047@gmail.com>
2025-01-15 19:16:04 +0300

    Add JWT token generation and validation (only username)

---
+++ src/main/java/com/dolgikh/scriptorium/security/JWTUtil.java
@@ -0,0 +1,29 @@
+package com.dolgikh.scriptorium.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JWTUtil {
+    @Value("${jwt_secret}")
+    private String secret;
+
+    public String generateToken(String username) {
+        return JWT
+                .create()
+                .withClaim("username", username)
+                .sign(Algorithm.HMAC256(secret));
+    }
+
+    public String validateToken(String token) {
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret)).build();
+
+        DecodedJWT jwt = verifier.verify(token);
+
+        return jwt.getClaim("username").asString();
+    }
+}
